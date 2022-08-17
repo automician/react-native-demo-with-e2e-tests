6ec98c0dd7e57a642d7c6ff59029474497cadc83
yashaka <yashaka@gmail.com>
2022-08-17 13:20:38 +0300

    update jest to 28.1 to use jest-circus test runner
    - that is a default jest test runner from 27.0
    - allowing to handle events like test_fn_failure in custom environments (https://jestjs.io/ru/docs/configuration#testenvironment-string)
      - see all events at https://github.com/facebook/jest/blob/79b5e41efa4878dfea87b2878ceafce42284fcec/packages/jest-types/src/Circus.ts#L86
    
    P.S.
    the main fix was specifying {rootMode: 'upward'} for babel-jest in jest.config.js
    - but also was installed @babel/preset-env (as a clue from docs https://jestjs.io/docs/getting-started#using-babel)
        - yet not sure whether this @babel/preset-env was needed... would be good to check this one day... (kind of TODO)

--- __e2e__/jest.config.js
+++ __e2e__/jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   testEnvironment: 'node',
   testTimeout: 90000,
   testRegex: '\\.test\\.(js|ts)$',
+  transform: {'\\.[jt]sx?$': ['babel-jest', {rootMode: 'upward'}]},
   verbose: true,
   setupFiles: ['./global.js', './setup.selenidejs.extensions.js'],
   setupFilesAfterEnv: ['./setup.appium.js'],
